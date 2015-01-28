ebcb3a7e0a7f9e44bdc8ff0724224010e0bba63a
Joost Molenaar <j.j.molenaar@gmail.com>
2015-01-28 22:16:52 +0100

    Stomrin web app

---
+++ stomrin.py
@@ -0,0 +1,62 @@
+
+import os
+import os.path
+
+import xhttp
+
+STOMRIN_DIR = os.environ.get('STOMRIN_DIR', '.')
+print 'STOMRIN_DIR:', STOMRIN_DIR
+
+class StomrinRoot(xhttp.Resource):
+    @xhttp.get({ 'postcode?': r'^[0-9]{4}\+[A-Za-z]{2}$',
+                 'huisnr?': r'^[1-9][0-9]*$',
+                 'toevoeging?': r'[A-Za-z0-9]*',
+                 'jaar?': '2[0-9]{3}$' })
+    def GET(self, req):
+        if req['x-get']['postcode'] and req['x-get']['huisnr'] and req['x-get']['jaar']:
+            req['x-get']['postcode'] = req['x-get']['postcode'].replace(' ', '').lower()
+            #print req['x-get']
+            basename = req['x-get']['jaar'] + '-' + req['x-get']['postcode'] + '-' + req['x-get']['huisnr']
+            if req['x-get']['toevoeging']: basename += '-' + req['x-get']['toevoeging']
+            #print `basename`
+            if os.path.exists(os.path.join(STOMRIN_DIR, basename) + '.plz'):
+                return { 'x-status': xhttp.status.ACCEPTED }
+            if os.path.exists(os.path.join(STOMRIN_DIR, basename) + '.acc'):
+                return { 'x-status': xhttp.status.ACCEPTED }
+            if os.path.exists(os.path.join(STOMRIN_DIR, basename) + '.err'):
+                with open(os.path.join(STOMRIN_DIR, basename) + '.err') as err:
+                    return { 'x-status': xhttp.status.NOT_FOUND,
+                             'x-content': err.read(),
+                             'content-type': 'text/plain' }
+            if os.path.exists(os.path.join(STOMRIN_DIR, basename) + '.html'):
+                return { 'x-status': xhttp.status.SEE_OTHER,
+                         'location': basename + '.html' }
+            with open(os.path.join(STOMRIN_DIR, basename) + '.plz', 'w'):
+                return { 'x-status': xhttp.status.ACCEPTED }
+        else:
+            return xhttp.serve_file('index.html', 'application/xhtml+xml')
+
+    def POST(self, req):
+        pass
+
+class StomrinRouter(xhttp.Router):
+    def __init__(self):
+        super(StomrinRouter, self).__init__(
+            ('^/$',                     xhttp.Redirector('/stomrin/')),
+            ('^/stomrin/$',             StomrinRoot()),
+            ('^/stomrin/(.*\.html)$',   xhttp.FileServer(STOMRIN_DIR, 'application/xhtml+xml'))
+        )
+
+class StomrinApp(StomrinRouter):
+    @xhttp.xhttp_app
+    @xhttp.catcher
+    def __call__(self, req, *a, **k):
+        #print '->', req
+        res = super(type(self), self).__call__(req, *a, **k)
+        #print '<-', res
+        return res
+
+app = StomrinApp()
+
+if __name__ == '__main__':
+    xhttp.run_server(app)
